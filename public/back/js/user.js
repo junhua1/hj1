a6970fa03f77c08c5ac7c52491ed3ee199605c85
hj <377390548@qq.com>
2017-10-30 14:54:33 +0800

    用户管理模块完成60%

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,3 +1,41 @@
 /**
  * Created by hua on 2017/10/30.
  */
+$(function () {
+    //发送ajax请求，获取后台的数据
+    var currentPage = 1;
+    var pageSize = 5;
+
+    //去后台获取数据，拿的currentPage页的数据
+    function render() {
+        $.ajax({
+            type:"get",
+            url:"/user/queryUser",
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            success:function (data) {
+                console.log(data);
+                var html = template("tpl",data);
+                $("tbody").html(html);
+
+                //分页功能
+                $("#paginator").bootstrapPaginator({
+                    bootstrapMajorVersion:3,
+                    currentPage:currentPage,
+                    size: "small",
+                    //向上取整
+                    totalPages: Math.ceil(data.total/data.size),
+                    onPageClicked:function(event, originalEvent, type,page){
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        currentPage = page;
+                        render();
+                    }
+                })
+            }
+        })
+    }
+    render();
+
+});
