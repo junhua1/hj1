1ab404cf9db02c9c18737159616af242fd4ca6e1
hj <377390548@qq.com>
2017-11-01 20:37:43 +0800

    乐淘首页与分类完成

---
+++ public/front/js/category.js
@@ -0,0 +1,40 @@
+/**
+ * Created by hua on 2017/11/1.
+ */
+var sc = mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005,
+    indicators:false
+});
+
+// 如果有多个 dom对象调用他时,返回值是一个数组
+
+
+//渲染一级页面
+$.ajax({
+    type:"get",
+    url:"/category/queryTopCategory",
+    success:function (data) {
+        console.log(data);
+        $(".lt_category_l .mui-scroll").html(template("tpl",data));
+        renderSecond(data.rows[0].id);
+    }
+});
+
+
+function renderSecond(id) {
+    $.ajax({
+        type:"get",
+        url:"/category/querySecondCategory",
+        data:{id:id},
+        success:function (data) {
+            console.log(data);
+            $(".lt_category_r .mui-scroll").html(template("tpl2",data));
+        }
+    })
+}
+
+$(".lt_category_l ul").on("click","li",function () {
+    var id = $(this).data("id");
+    $(this).addClass("now").siblings().removeClass("now");
+    renderSecond(id);
+})
--- public/front/js/index.js
+++ public/front/js/index.js
@@ -8,5 +8,5 @@ mui('.mui-scroll-wrapper').scroll({
 
 //轮播图
 mui('.mui-slider').slider({
-    interval:1000 // 自动轮播周琦,若为0 不自动播放,默认为0
+    interval:1000 // 自动轮播周期,若为0 不自动播放,默认为0
 })
